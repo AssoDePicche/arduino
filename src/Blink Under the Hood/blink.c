32ba185a32474c965272be9ebb0dcbb38b1adb51
AssoDePicche <samuelprado730@gmail.com>
2023-04-20 19:36:42 -0300

    fix: add f_cpu to blink.c

--- src/Blink Under the Hood/blink.c
+++ src/Blink Under the Hood/blink.c
@@ -1,19 +1,16 @@
+#define F_CPU 16000000
+#define BLINK_DELAY 1000
+
 #include <avr/io.h>
 #include <util/delay.h>
 #include <stdbool.h>
 
-#define BLINK_DELAY 500
-
 int main(void)
 {
-  // Put B5 in output mode
-
   DDRB |= _BV(PORTB5);
 
   while (true)
   {
-    // Toggle the LED
-
     PORTB ^= _BV(PORTB5);
 
     _delay_ms(BLINK_DELAY);
